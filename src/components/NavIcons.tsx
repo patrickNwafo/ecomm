a1dc5a2a42afa8d613639e40a04e96c5db6b9381
ricky <patrickchinwafor@gmail.com>
2024-12-30 17:39:32 +0100

    NavIcons

--- src/components/NavIcons.tsx
+++ src/components/NavIcons.tsx
@@ -1,7 +1,42 @@
 "use client";
 
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
+
 const NavIcons = () => {
-    return <div className="">NavIcons</div>;
+    const [isProfileOpen, setIsProfileOpen] = useState(false);
+    const [isCartOpen, setIsCartOpen] = useState(false);
+    return (
+        <div className="flex items-center gap-4 xl:gap-6">
+            <Image
+                src="/profile.png"
+                alt=""
+                width={22}
+                height={22}
+                className="cursor-pointer"
+            />
+            {isProfileOpen && (
+                <div className="">
+                    <Link href="/"></Link>
+                </div>
+            )}
+            <Image
+                src="/notification.png"
+                alt=""
+                width={22}
+                height={22}
+                className="cursor-pointer"
+            />
+            <Image
+                src="/cart.png"
+                alt=""
+                width={22}
+                height={22}
+                className="cursor-pointer"
+            />
+        </div>
+    );
 };
 
 export default NavIcons;
