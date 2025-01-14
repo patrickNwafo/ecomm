ceefbbfdc986121804ea51eb720ff7ad2b5d6c32
ricky <patrickchinwafor@gmail.com>
2025-01-14 04:25:33 +0100

    product img 2

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,3 +1,5 @@
+import Add from "@/components/Add";
+import CustomizeProducts from "@/components/CustomizeProducts";
 import ProductImages from "@/components/ProductImages";
 
 const SinglePage = () => {
@@ -8,7 +10,55 @@ const SinglePage = () => {
                 <ProductImages />
             </div>
             {/* TEXTS */}
-            <div className="w-full lg:w-1/2 flex flex-col gap-6">TEXTS</div>
+            <div className="w-full lg:w-1/2 flex flex-col gap-6">
+                <h1 className="text-4xl font-medium">Product Name</h1>
+                <p className="text-gray-500">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                    Iusto maxime, quisquam sequi maiores error culpa hic
+                    corporis reiciendis saepe eaque, soluta explicabo
+                    reprehenderit! Voluptatibus ipsum illo quaerat commodi
+                    pariatur dignissimos.
+                </p>
+                <div className="h-[20px] bg-gray-100" />
+                <div className="flex gap-4 items-center">
+                    <h3 className="text-xl text-gray-500 line-through">$59</h3>
+                    <h2 className="font-medium text-2xl">$49</h2>
+                </div>
+                <div className="h-[20px] bg-gray-100" />
+                <CustomizeProducts />
+                <Add />
+                <div className="h-[20px] bg-gray-100" />
+                <div className="text-sm">
+                    <h4 className="font-medium mb-4">Title</h4>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Ducimus nobis veniam nemo quisquam enim minus maxime
+                        repellendus? Reprehenderit veniam voluptatibus quibusdam
+                        voluptates praesentium? Explicabo voluptatem
+                        reprehenderit facere, soluta quasi alias.
+                    </p>
+                </div>
+                <div className="text-sm">
+                    <h4 className="font-medium mb-4">Title</h4>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Ducimus nobis veniam nemo quisquam enim minus maxime
+                        repellendus? Reprehenderit veniam voluptatibus quibusdam
+                        voluptates praesentium? Explicabo voluptatem
+                        reprehenderit facere, soluta quasi alias.
+                    </p>
+                </div>
+                <div className="text-sm">
+                    <h4 className="font-medium mb-4">Title</h4>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Ducimus nobis veniam nemo quisquam enim minus maxime
+                        repellendus? Reprehenderit veniam voluptatibus quibusdam
+                        voluptates praesentium? Explicabo voluptatem
+                        reprehenderit facere, soluta quasi alias.
+                    </p>
+                </div>
+            </div>
         </div>
     );
 };
---
+++ src/components/Add.tsx
@@ -0,0 +1,5 @@
+const Add = () => {
+    return <div className="">Add</div>;
+};
+
+export default Add;
